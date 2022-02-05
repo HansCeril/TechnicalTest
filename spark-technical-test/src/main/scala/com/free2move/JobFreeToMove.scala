fd9b360f76b1d57df13b591f4301461628f254a2
CERIL Hans Ext DTSI/PFS <QFZF0680@orange.com>
2022-02-06 00:20:06 +0100

    add statistics

--- spark-technical-test/src/main/scala/com/free2move/JobFreeToMove.scala
+++ spark-technical-test/src/main/scala/com/free2move/JobFreeToMove.scala
@@ -1,8 +1,8 @@
 package com.free2move
 import com.free2move.models.CsvModelSchema
-
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{avg, col, count, round, sum}
 
 
 
@@ -17,7 +17,6 @@ object JobFreeToMove {
 
   def main(args: Array[String]): Unit = {
     run()
-
   }
 
 
@@ -27,8 +26,6 @@ object JobFreeToMove {
       .appName(SPARK_APP_NAME)
       .config("spark.master", "local")
       .getOrCreate()
-
-    spark.sparkContext.setLogLevel("DEBUG")
 
     val schemaLoader = new CsvModelSchema()
 
@@ -50,6 +47,67 @@ object JobFreeToMove {
 
 
     // =========================================== Statistics =================================================
+    //
+
+    //==========================================
+    // Best Seller
+    // Which User spent the most today ?
+    val BestSeller = itemsDataSet
+      .groupBy("seller_id")
+      .agg(round(sum("price"), 2))
+      .alias("total_spent")
+      .withColumnRenamed("round(sum(price), 2)", "Total Item Sale")
+    BestSeller.show()
+
+
+
+    // ====================================================================
+
+    val itemsMostSold = itemsDataSet
+      .groupBy("product_id")
+      .agg(count("product_id"), round(sum("price"), 2))
+      .alias("total_spent")
+      .withColumnRenamed("round(sum(price), 2)", "Total_Sales_items")
+      .withColumnRenamed("count(product_id)", "Number_sold_product")
+      .withColumnRenamed("product_id", "n_product_id")
+      .orderBy(col("round(sum(price), 2)").desc)
+      .limit(15)
+    itemsMostSold.show()
+
+    val productMostSold = productsDataSet
+      .join(itemsMostSold, productsDataSet.col("product_id").equalTo(itemsMostSold.col("n_product_id")))
+      .select("product_id", "product_category_name", "Total_Sales_items", "Number_sold_product")
+
+    productMostSold.show()
+
+
+
+    // ========================================= Best Customer ==================================
+
+    val ordersConfirm = ordersDataSet
+      .filter(col("order_status") === "delivered")
+      .select("order_id","customer_id")
+
+    ordersConfirm.show()
+
+    val bestOrderItemPrice = itemsDataSet
+      .select("order_id", "price")
+      .orderBy(col("price").desc)
+    bestOrderItemPrice.show()
+
+
+    val mergeBestPriceOrder = ordersConfirm
+      .join(bestOrderItemPrice, ordersConfirm.col("order_id").equalTo(bestOrderItemPrice.col("order_id")), "inner")
+    mergeBestPriceOrder.show()
+
+
+
+
+
+
+
+
+
   }
 
   private def readCsvIntoDataframe(s: SparkSession, filename: String, schema: StructType) = {
