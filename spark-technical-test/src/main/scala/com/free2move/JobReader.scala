0ef36c1d85ce48470fb55f19d97a1a2b9eed66c1
Hans Ceril <hans.ceril.ext@orange.com>
2022-02-05 00:33:07 +0000

    create RDD for each CSV file

--- spark-technical-test/src/main/scala/com/free2move/JobReader.scala
+++ spark-technical-test/src/main/scala/com/free2move/JobReader.scala
@@ -8,6 +8,13 @@ object JobReader {
   case class Customer(customer_id: String, customer_unique_id: String, customer_zip_code_prefix: String, //
         customer_city: String, customer_state: String)
 
+  case class Items(order_id: String, order_item_id: String, product_id: String, //
+                   seller_id: String, shipping_limit_date: String, price: String, freight_value: String)
+
+  case class Orders(order_id: String, customer_id: String, order_status: String, //
+                    order_purchase_timestamp: String, order_approved_at: String, order_delivered_carrier_date: String, //
+                    order_delivered_customer_date: String, order_estimated_delivery_date: String)
+
   def main(args: Array[String]) {
 
     var conf = new SparkConf().setAppName("Read CSV FIle").setMaster("local[*]")
@@ -15,10 +22,11 @@ object JobReader {
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
+    val fileList = Array("data/customer.csv", "data/items.csv", "data/orders.csv", "data/products.csv")
 
+
+    // ===================================  Rdd Customers  ==========================================
     val rddFromFileCustomers = sc.textFile("data/customer.csv")
-
-
     val empRddCustomers  = rddFromFileCustomers.map {
       line =>
         val column = line.split(",")
@@ -30,9 +38,47 @@ object JobReader {
           column(4)
         )
     }
-
     val df_customers = empRddCustomers.toDF()
     df_customers.show()
+
+
+
+    // ============================================ RDD Items ============================================
+    val rddFromFileItems = sc.textFile("data/items.csv")
+    val empRddItems  = rddFromFileItems.map {
+      line =>
+        val column = line.split(",")
+        Items(
+          column(0),
+          column(1),
+          column(2),
+          column(3),
+          column(4),
+          column(5),
+          column(6),
+        )
+    }
+    val df_items = empRddItems.toDF()
+    df_items.show()
+
+    // ==============================================  RDD orders  ==============================================
+    val rddFromFileOrders = sc.textFile("data/items.csv")
+    val empRddOrders  = rddFromFileOrders.map {
+      line =>
+        val column = line.split(",")
+        Orders(
+          column(0),
+          column(1),
+          column(2),
+          column(3),
+          column(4),
+          column(5),
+          column(6),
+          column(7)
+        )
+    }
+    val df_orders = empRddOrders.toDF()
+    df_orders.show()
 
     //rdd.foreach(f=>{
     //  println("Col1:"+f(0)+",Col2:"+f(1))
